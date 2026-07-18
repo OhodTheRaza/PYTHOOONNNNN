808a3292ca1b43c7f30d4cd40c83a98ecd58280b
OhodTheRaza <muhammadohodraza25@gmail.com>
2026-07-18 16:01:17 +0500

    Add files via upload

---
+++ Birthdayprojectafterclassword.py
@@ -0,0 +1,20 @@
+John = "John's Birthday is on "
+JohnBday = "2014 November 31"
+JohnPf = John + JohnBday
+print(JohnPf)
+Joshua = "Joshua's Birthday is on "
+JoshuaBday = "2013 December 13"
+JoshuaPf = Joshua + JoshuaBday
+print(JoshuaPf)
+MichaelJackson = "Michael Jackson's Birthday is on "
+MichaeJBday = "2014 December 9"
+MichaelJPf = Joshua + JoshuaBday
+print(MichaelJPf)
+Horace = "Horace's Birthday is on "
+HoraceBday = "2013 December 13"
+HoracePf = Horace + HoraceBday
+print(HoracePf)
+Juan = "Juan's Birthday is on "
+JuanBday = "2013 December 13"
+JuanPf = Juan + JuanBday
+print(JuanPf)
